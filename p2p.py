54ce55ab15441f1f41e1320d2fed537297486632
sheemer <38991819+sheemer@users.noreply.github.com>
2024-06-20 21:30:50 -0500

    Add files via upload
    finished

--- p2p.py
+++ p2p.py
@@ -4,39 +4,70 @@ import threading
 import rsa
 
 
-#Need security
-publicKey, privateKey = rsa.newkeys(512)
+publicKey, privateKey = rsa.newkeys(2048)
 
-#TODO: exit program when client ends the connection
+with open("pubkey_master.pem", "wb") as f:
+    f.write(publicKey.save_pkcs1("PEM"))
+    
 def connect(conn):
     while True:
         received = conn.recv(1024)
-        decmess = rsa.decrypt(received.decode(),privateKey)
+        
         if received ==' ':
             pass
-        if received == '-----BEGIN RSA PUBLIC KEY-----':
-            
-        #Not working
-            #-----------------------------------------------
         else:
-            print("Sender:",decmess)
+            decmess = rsa.decrypt(received, privateKey)
+            print(decmess)
 
 def sendMsg(conn):
     while True:
-        send_msg = input().replace('b', '')
-        encMessage = rsa.encrypt(send_msg.encode(),publicKey)
+        send_msg = input().replace('b', '').encode()
+        encMessage = rsa.encrypt(send_msg,clientkey)
         if send_msg == ' ':
             pass
         else:
-            conn.sendall(encMessage)
+            conn.send(encMessage)
 
 if __name__ == '__main__':
+    password = input('Please enter chat password: ')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('192.168.1.248', 12345))
     s.listen(5)
     (conn, addr) = s.accept() 
-    print ('Got connection from', addr)
+    print ('Got connection from: ', addr)
+    
+    
+    #Checking If passwords match.
+    clientpws = conn.recv(1024).decode()
+    if password == clientpws:
+        print("Client password success. Happy chatting!")
+        pass
+    else: 
+        print("Client password fail.")
+        sys.exit() 
+   
+    
+    
+    #MASTER recieving the Client PUBKEY
+    f = open("public_client.pem",'wb')
+    l = conn.recv(1024)
+    f.write(l)
+    f.close()
+     
+    clientkey = rsa.PublicKey.load_pkcs1(l)
+    
+ #MASTER Sending the MASTER PUBKEY
+    t = open("pubkey_master.pem",'rb')
+    p = t.read(1024)
+    conn.send(p)
+ 
+    
+
+    
+    
+    
+       
     thread1 = threading.Thread(target = connect, args = ([conn]))
     thread2 = threading.Thread(target = sendMsg, args = ([conn]))
     thread1.start()
--- p2p2.py
+++ p2p2.py
@@ -6,54 +6,61 @@ import rsa
 
 # Need security
 
-publickey, privateKey = rsa.newkeys(512)
-print(publickey)
+publickey, privateKey = rsa.newkeys(2048)
+
 
 with open("public_client.pem", "wb") as f:
     f.write(publickey.save_pkcs1("PEM"))
     
 with open("public_client.pem", "rb") as file:
     content = file.read()
-    print(content)
-    
-    
-#with open("public_server.pem", "rb") as f:
-#    public_key_server = rsa.PublicKey.load_pkcs1(f.read())
-
-#TODO:end connection with 'exit'
+  
 def connect(s):
     while True:
         r_msg = s.recv(1024)
-        decmess = rsa.decrypt(r_msg.decode(),privateKey)
+        decmess = rsa.decrypt(r_msg,privateKey)
 
         if not r_msg:
             break
         if r_msg == '':
             pass
         else:
-            print("Sender:",decmess)
+            print(decmess)
 
 
 def receive(s):
     while True:
-        s_msg = input().replace('b','')
-        encMessage = rsa.encrypt(s_msg.encode(),publickey)
+        s_msg = input().replace('b','').encode()
+        encMessage = rsa.encrypt(s_msg, masterkeypub)
         if s_msg == '':
             pass
-        #if s_msg.decode() == 'exit':
-         #   print("wan exit")
-          #  break
         else:
-            s.sendall(encMessage)
+            s.send(encMessage)
 
 if __name__ == '__main__':
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.connect((sys.argv[1], int(sys.argv[2])))
-    # not working
-    #file = open("public_client.pem" , "rb")
-    s.send(content)
+    
+    #Password to send to the server.
+    password = input("Please enter chat password:  ").encode()
+    s.send(password)
+   
+   
+   #Client sending PUBKEY
+    f = open('public_client.pem','rb')
+    l = f.read(1024)
+    s.send(l)
+    
+    
+    #Client recieving the MASTERPUBKEY
+    t = open("pubkey_master.pem",'wb')
+    p = s.recv(1024)
+    t.write(p)
+    t.close() 
+    masterkeypub = rsa.PublicKey.load_pkcs1(p)
+    
     #--------------------------------------------------------------------------
     thread1 = threading.Thread(target = connect, args = ([s]))
     thread2 = threading.Thread(target = receive, args = ([s]))
